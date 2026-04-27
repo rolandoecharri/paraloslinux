c2eae3b8ae60ec949512cc382963c8931bd478ca
Rolando <rolandoecharri@gmail.com>
2026-04-27 16:47:03 -0300

    first commit

---
+++ setup_ljansible.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+USERNAME="ljansible"
+SSH_PUB_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN9tAu/9cZIZXAn6H02WC/VeBFjZh/720ENshziDqIN0 ljansible"
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Ejecutar como root: sudo bash $0"
+  exit 1
+fi
+
+useradd -m -s /bin/bash "$USERNAME"
+
+echo "Ingresa la contraseña para $USERNAME:"
+passwd "$USERNAME"
+
+if ! grep -q "^${USERNAME} ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
+  echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+fi
+
+mkdir -p /home/${USERNAME}/.ssh
+echo "$SSH_PUB_KEY" >> /home/${USERNAME}/.ssh/authorized_keys
+chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.ssh
+chmod 700 /home/${USERNAME}/.ssh
+chmod 600 /home/${USERNAME}/.ssh/authorized_keys
+
+echo "Usuario $USERNAME creado y configurado correctamente."
